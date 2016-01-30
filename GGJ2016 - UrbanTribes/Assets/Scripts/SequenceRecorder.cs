f2e2b939b7bdd1117a69f535a46d43a91b7afb55
Ramiro Hernandez Dorado <idloco@live.com>
2016-01-30 02:10:15 -0600

    Sequece Recorder functional

--- GGJ2016 - UrbanTribes/Assets/Scripts/SequenceRecorder.cs
+++ GGJ2016 - UrbanTribes/Assets/Scripts/SequenceRecorder.cs
@@ -7,14 +7,22 @@ namespace Assets.Scripts
     public class SequenceRecorder : MonoBehaviour
     {
 
-        public List<SequenceItem> SequenceItems;
+        public List<SequenceItem> CurrentKeySequence;
+        public List<SequenceItem> OldKeySequence;
+        
+        public Text DisplayText;
+        public int MaxSequenceSize = 10;
+        public float CoolDownInSeconds = 0.2f;
+
 
         private bool isRecording = false;
+        private bool isImitating = false;
+
+
         private bool isPressingKey = false;
         private float startedRecodingTime;
-
-        public Text DisplayText;
-
+        
+        
 
         private static KeyCode[] validKeys =
         {
@@ -22,24 +30,31 @@ namespace Assets.Scripts
             KeyCode.Joystick1Button1,
             KeyCode.Joystick1Button2,
             KeyCode.Joystick1Button3,
-            KeyCode.Joystick1Button4
+            KeyCode.Joystick1Button4,
+            KeyCode.Joystick1Button5,
+            KeyCode.Joystick1Button6,
+            KeyCode.Joystick1Button7
+
         };
 	
         // Use this for initialization
         public void Start ()
         {
-	        SequenceItems = new List<SequenceItem>();
+	        CurrentKeySequence = new List<SequenceItem>();
             isRecording = false;
             isPressingKey = false;
             startedRecodingTime = 0;
+            MaxSequenceSize = 10;
         }
 	
         // Update is called once per frame
         public void Update ()
         {
 
-            if(isRecording) return;
-            
+            //if(isRecording == false) return;
+
+            if (CurrentKeySequence.Count >= MaxSequenceSize) return;
+
 
             foreach (var key in validKeys)
             {
@@ -47,36 +62,71 @@ namespace Assets.Scripts
                 {
                     isPressingKey = true;
                     var newKey = new SequenceItem(key, Time.timeSinceLevelLoad - startedRecodingTime);
-                    SequenceItems.Add(newKey);
+                    CurrentKeySequence.Add(newKey);
+                    Debug.Log("Size>> "+CurrentKeySequence.Count   + "  Max:  " + MaxSequenceSize);
                 }
             }
 
+            
+
             DisplayText.text = KeySequenseToString();
-
-
+        
         }
 
         public void StopRecording()
         {
             isRecording = false;
+            
         }
 
         public void StartRecording()
         {
             isRecording = true;
+            isImitating = false;
             startedRecodingTime = Time.timeSinceLevelLoad;
         }
+
+        public void StartImitating()
+        {
+            isImitating = true;
+            isRecording = false;
+            startedRecodingTime = Time.timeSinceLevelLoad;
+        }
+
+        public void ClearSequence()
+        {
+            CurrentKeySequence.Clear();
+            MaxSequenceSize = 10;
+        }
+
 
         private string KeySequenseToString()
         {
             var sequence = "";
-            foreach (var key in SequenceItems)
+            foreach (var key in CurrentKeySequence)
             {
-                sequence += key.KeyPressed.ToString() + "\n" ;
+                sequence += key.KeyPressed.ToString() + " - " + key.TimePressed  + "\n" ;
             }
 
             return sequence;
         }
 
+
+        public int CompareSequence(List<SequenceItem> baseSequence, List<SequenceItem> resultSequence)
+        {
+
+            for (int i = 0; i < baseSequence.Count; i++)
+            {
+
+                //baseSequence[i]
+            }
+
+
+            return 0;
+        }
+
+
+
     }
+
 }
