f2eaacac76dc9126dc95c6d17a9a91894f9d8574
Juan Pablo Navarro Fennell <jnavarro@npmaudiovisual.com>
2016-01-30 01:37:00 -0600

    Base de menus
    scripts miselaneos

---
+++ GGJ2016 - UrbanTribes/Assets/Scripts/Utility/MainMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public string creditsSceneName;
+	public string mainMenuSceneName;
+	public string gameSceneName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void DisplayCredts(int scene) {
+		switch (scene) {
+		case 0:
+			SceneManager.LoadScene (creditsSceneName);
+			break;
+		case 1:
+			SceneManager.LoadScene (mainMenuSceneName);
+			break;
+		case 2:
+			SceneManager.LoadScene (gameSceneName);
+			break;
+		}
+	}
+}
