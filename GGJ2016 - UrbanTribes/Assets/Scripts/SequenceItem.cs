2d41eb3194bb23bda01ac643eb3744dc01a1ebb3
Ramiro Hernandez Dorado <idloco@live.com>
2016-01-30 00:41:21 -0600

    Added Sequence Manager

---
+++ GGJ2016 - UrbanTribes/Assets/Scripts/SequenceItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SequenceItem
+    {
+        public KeyCode KeyPressed;
+        public float TimePressed;
+
+        public SequenceItem(KeyCode key, float timePressed)
+        {
+            KeyPressed = key;
+            TimePressed = timePressed;
+        }
+    }
+}
--- GGJ2016 - UrbanTribes/Assets/Scripts/SequenceRecorder.cs
+++ GGJ2016 - UrbanTribes/Assets/Scripts/SequenceRecorder.cs
@@ -1,15 +1,82 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-public class SequenceRecorder : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class SequenceRecorder : MonoBehaviour
+    {
 
-	// Use this for initialization
-	void Start () {
+        public List<SequenceItem> SequenceItems;
+
+        private bool isRecording = false;
+        private bool isPressingKey = false;
+        private float startedRecodingTime;
+
+        public Text DisplayText;
+
+
+        private static KeyCode[] validKeys =
+        {
+            KeyCode.Joystick1Button0,
+            KeyCode.Joystick1Button1,
+            KeyCode.Joystick1Button2,
+            KeyCode.Joystick1Button3,
+            KeyCode.Joystick1Button4
+        };
 	
-	}
+        // Use this for initialization
+        public void Start ()
+        {
+	        SequenceItems = new List<SequenceItem>();
+            isRecording = false;
+            isPressingKey = false;
+            startedRecodingTime = 0;
+        }
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        // Update is called once per frame
+        public void Update ()
+        {
+
+            if(isRecording) return;
+            
+
+            foreach (var key in validKeys)
+            {
+                if (Input.GetKeyUp(key))
+                {
+                    isPressingKey = true;
+                    var newKey = new SequenceItem(key, Time.timeSinceLevelLoad - startedRecodingTime);
+                    SequenceItems.Add(newKey);
+                }
+            }
+
+            DisplayText.text = KeySequenseToString();
+
+
+        }
+
+        public void StopRecording()
+        {
+            isRecording = false;
+        }
+
+        public void StartRecording()
+        {
+            isRecording = true;
+            startedRecodingTime = Time.timeSinceLevelLoad;
+        }
+
+        private string KeySequenseToString()
+        {
+            var sequence = "";
+            foreach (var key in SequenceItems)
+            {
+                sequence += key.KeyPressed.ToString() + "\n" ;
+            }
+
+            return sequence;
+        }
+
+    }
 }
