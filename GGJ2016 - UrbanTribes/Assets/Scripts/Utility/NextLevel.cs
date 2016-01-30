aade7f2ad081cc8ae443ce6cbb45a66ac2d89170
Juan Pablo Navarro Fennell <jnavarro@npmaudiovisual.com>
2016-01-30 00:55:32 -0600

    Cambios Varios

---
+++ GGJ2016 - UrbanTribes/Assets/Scripts/Utility/NextLevel.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NextLevel : MonoBehaviour {
+
+	public int timer;
+	public string nextLevelName;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (NextLevelTimer ());
+	}
+
+	private IEnumerator NextLevelTimer () {
+		yield return new WaitForSeconds (timer);
+	}
+}
