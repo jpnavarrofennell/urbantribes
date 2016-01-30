6f99be806eea3732cf0076236c96fee4ba58c02c
Juan Pablo Navarro Fennell <jnavarro@npmaudiovisual.com>
2016-01-30 10:46:33 -0600

    Animations del box moves
    Animaciones varias

--- GGJ2016 - UrbanTribes/Assets/Scripts/GameScripts/MoveBox.cs
+++ GGJ2016 - UrbanTribes/Assets/Scripts/GameScripts/MoveBox.cs
@@ -4,13 +4,28 @@ using System.Collections;
 
 public class MoveBox : MonoBehaviour {
 
+	public Animator box;
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine (AnimSwitch ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	private IEnumerator AnimSwitch() {
+		int i = 0;
+		while (true) {
+			yield return new WaitForSeconds (1f);
+			box.SetInteger ("estado",i);
+
+			i++;
+			if (i > 3) {
+				i = 0;
+			}
+		
+		}
 	}
 }
